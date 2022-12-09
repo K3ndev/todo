f9a080f35abc28965e0a989b3f2893955c42cc9b
k3n <racelisjk@gmail.com>
2022-12-09 21:53:48 +0800

    removed mantine - modal

--- components/modal/index.tsx
+++ components/modal/index.tsx
@@ -1,35 +1,81 @@
 /* eslint-disable @next/next/no-img-element */
-import { useState, useEffect } from 'react';
-import { Modal } from '@mantine/core';
-import { useTodoStore } from '../../store/todoStore'
-import { useDebouncedValue } from '@mantine/hooks';
+import { useState, useEffect } from "react";
+import { useTodoStore } from "../../store/todoStore";
+import { useDebouncedValue } from "@mantine/hooks";
 
 export const ModalLogin = () => {
+  // states
+  const [opened, setOpened] = useState(true);
+  // name
+  const [inputName, setInputName] = useState<string>("");
+  const [nameDeb] = useDebouncedValue(inputName, 200);
 
-    // states
-    const [opened, setOpened] = useState(true);
-    // name 
-    const [inputName, setInputName] = useState<string>('')
-    const [nameDeb] = useDebouncedValue(inputName, 200);
+  // store
+  const { name, addName } = useTodoStore<any>((states: any) => states);
 
-    // store
-    const { name, addName } = useTodoStore<any>((states: any) => states)
+  // input fN
+  function inputNameHandler(e: React.ChangeEvent<HTMLInputElement>) {
+    setInputName(e.target.value);
+  }
 
-    // input fN
-    function inputNameHandler(e: React.ChangeEvent<HTMLInputElement>) {
-        setInputName(e.target.value)
-    }
+  //   modal state
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(true);
 
-    useEffect(() => {
-        setTimeout(() => {
-            addName(nameDeb)
-        }, 300)
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [nameDeb])
+  //   modal Fn
+  const modalHandler = () => {
+    setIsModalOpen(!isModalOpen);
+  };
 
-    return (
-        <>
-            <Modal
+  useEffect(() => {
+    setTimeout(() => {
+      addName(nameDeb);
+    }, 300);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [nameDeb]);
+
+  return (
+    <>
+      <div className={`absolute inset-0 ${isModalOpen ? "block" : "hidden"}`}>
+        <div className="flex items-center justify-center h-full">
+          <div className="w-screen h-screen  md:w-[31.25rem] md:h-[31.25rem] flex flex-col p-10 custom-gradient">
+            <div className="h-[30.125rem] flex justify-center items-center drop-shadow-xl">
+              <img src="./logo.png" alt="logo" className="" />
+            </div>
+
+            <div className="flex flex-col justify-between w-full h-full gap-2">
+              <div className="w-full">
+                <h1 className="text-2xl font-bold md:text-xl">
+                  Welcome to Dona Clone
+                </h1>
+                <p className="text-base md:text-sm text-[#808080] md:w-[65%]">
+                  Dona is a back to-do list focused on fast and delightful user
+                  experience.
+                </p>
+              </div>
+
+              <div className="flex flex-col w-full">
+                <h1 className="text-base">What&apos;s your name?</h1>
+                <input
+                  autoComplete="off"
+                  value={inputName}
+                  onChange={inputNameHandler}
+                  type="text"
+                  placeholder="Type it here"
+                  className="outline-none focus:border-b-2 w-[40%] text-[#195786] text-sm h-7"
+                />
+                <button
+                  onClick={modalHandler}
+                  className="w-[34%] text-center bg-[#008FFD] hover:bg-[#0276cf] p-2 text-white rounded-[0.625rem] mt-10 text-base"
+                >
+                  <div>Continue</div>
+                </button>
+                <div className="w-full md:hidden h-36"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <Modal
                 // animation 
                 transition="fade"
                 transitionDuration={600}
@@ -51,11 +97,11 @@ export const ModalLogin = () => {
 
                     <div className='flex flex-col justify-between w-full h-full gap-2'>
                         <div className='w-full'>
-                            <h1 className='text-2xl md:text-xl font-bold'>Welcome to Dona Clone</h1>
+                            <h1 className='text-2xl font-bold md:text-xl'>Welcome to Dona Clone</h1>
                             <p className='text-base md:text-sm text-[#808080] md:w-[65%]'>Dona is a back to-do list focused on fast and delightful user experience.</p>
                         </div>
 
-                        <div className='w-full flex flex-col'>
+                        <div className='flex flex-col w-full'>
                             <h1 className='text-base'>What&apos;s your name?</h1>
                             <input autoComplete="off" value={inputName} onChange={inputNameHandler} type="text" placeholder='Type it here' className='outline-none focus:border-b-2 w-[40%] text-[#195786] text-sm h-7' />
                             <button onClick={() => setOpened(!opened)} className='w-[34%] text-center bg-[#008FFD] hover:bg-[#0276cf] p-2 text-white rounded-[0.625rem] mt-10 text-base'>
@@ -63,11 +109,11 @@ export const ModalLogin = () => {
                                     Continue
                                 </div>
                             </button>
-                            <div className='md:hidden w-full h-36'></div>
+                            <div className='w-full md:hidden h-36'></div>
                         </div>
                     </div>
                 </div>
-            </Modal>
-        </>
-    );
-}
+            </Modal> */}
+    </>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,29 @@
 /* eslint-disable @next/next/no-img-element */
-import type { NextPage } from 'next'
-import Layout from '../layout/index'
-import Head from 'next/head'
-import { TodoList, Sidebar, ModalLogin } from '../components/index'
-import { format } from 'date-fns'
-import { useTodoStore } from '../store/todoStore'
+import type { NextPage } from "next";
+import Layout from "../layout/index";
+import Head from "next/head";
+import { TodoList, Sidebar, ModalLogin } from "../components/index";
+import { format } from "date-fns";
+import { useTodoStore } from "../store/todoStore";
 
 const Home: NextPage = () => {
-
-
-  // states 
-  const date = new Date()
-  const day = format(date, "iiii")
-  const month = format(date, "MMM")
-  const dayNum = format(date, "dd")
+  // states
+  const date = new Date();
+  const day = format(date, "iiii");
+  const month = format(date, "MMM");
+  const dayNum = format(date, "dd");
   const periodFN = () => {
-    const tempPeriod = format(date, "a")
-    if (tempPeriod === 'PM') {
-      return 'afternoon';
+    const tempPeriod = format(date, "a");
+    if (tempPeriod === "PM") {
+      return "afternoon";
     } else {
-      return 'morning'
+      return "morning";
     }
-  }
-  const period = periodFN()
+  };
+  const period = periodFN();
 
   // store
-  const { name } = useTodoStore<any>((states: any) => states)
+  const { name } = useTodoStore<any>((states: any) => states);
 
   return (
     <>
@@ -34,37 +32,37 @@ const Home: NextPage = () => {
       </Head>
 
       <Layout>
-
         {/* sidebar */}
         <Sidebar />
 
         {/* main todolist */}
-        <section className='flex flex-col lg:w-[66%] gap-7 w-full h-[90vh] items-center'>
-
-
-          <div className='w-full flex flex-col gap-4 md:w-[100%]'>
-
-            <div className='w-full flex justify-start mt-9'>
-              <div className='flex gap-5'>
-                <img src="/dona_Avatar.svg" alt="dona_Avatar" className='w-[25px] md:w-[35px]' />
-                <div className='w-full'>
+        <section className="flex flex-col lg:w-[66%] gap-7 w-full h-[90vh] items-center">
+          <div className="w-full flex flex-col gap-4 md:w-[100%]">
+            <div className="flex justify-start w-full mt-9">
+              <div className="flex gap-5">
+                <img
+                  src="/dona_Avatar.svg"
+                  alt="dona_Avatar"
+                  className="w-[25px] md:w-[35px]"
+                />
+                <div className="w-full">
                   <ModalLogin />
-                  <h1 className='text-xl	md:text-2xl lg:text-3xl font-normal text-black leading-none w-full'>Good {period}, {name.length == false ? 'Human' : name}</h1>
-                  <p className='text-base	md:text-base lg:text-2xl font-normal text-[#6D6D6D] leading-6'>It&apos;s  {day}, {month} {dayNum}   </p>
+                  <h1 className="w-full text-xl font-normal leading-none text-black md:text-2xl lg:text-3xl">
+                    Good {period}, {name.length == false ? "Human" : name}
+                  </h1>
+                  <p className="text-base	md:text-base lg:text-2xl font-normal text-[#6D6D6D] leading-6">
+                    It&apos;s {day}, {month} {dayNum}{" "}
+                  </p>
                 </div>
-
               </div>
             </div>
 
             <TodoList />
           </div>
         </section>
-
       </Layout>
-
-
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
